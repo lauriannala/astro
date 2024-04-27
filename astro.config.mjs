ecb09a35e846df573137d4c3f0181b19adbf99f0
Lauri Annala <lauri.annala@protonmail.com>
2024-04-27 08:30:45 +0300

    Vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
   integrations: [mdx()]
 });
